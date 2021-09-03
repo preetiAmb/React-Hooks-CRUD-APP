29bc545799675660fd3bebb790d105c228f60cf8
Preeti <35522305+preetiAmb@users.noreply.github.com>
2021-09-03 18:45:22 +0200

    Update AddForm.js

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -23,19 +23,19 @@ const AddForm = () => {
     <Form onSubmit={handleSubmit}>
       <Form.Group>
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Name *" value={name}name="name" onChange= { (e) => onInputChange(e)} required />
+        <Form.Control type="text" placeholder="Name *" value={name} name="name" onChange= { (e) => onInputChange(e)} required />
       </Form.Group>
       <Form.Group>
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Email *" value={email}name="email" onChange= { (e) => onInputChange(e)} required />
+        <Form.Control type="email" placeholder="Email *" value={email} name="email" onChange= { (e) => onInputChange(e)} required />
       </Form.Group>
       <Form.Group>
         <Form.Label>Address</Form.Label>
-        <Form.Control type="text" placeholder="Address" value={address}name="address" onChange= { (e) => onInputChange(e)} />
+        <Form.Control type="text" placeholder="Address" value={address} name="address" onChange= { (e) => onInputChange(e)} />
       </Form.Group>
       <Form.Group>
         <Form.Label>Phone</Form.Label>
-        <Form.Control type="text" placeholder="phone" value={phone}name="phone" onChange= { (e) => onInputChange(e)} />
+        <Form.Control type="text" placeholder="phone" value={phone} name="phone" onChange= { (e) => onInputChange(e)} />
       </Form.Group>
       <br />
       <Button variant="success" type="submit">
